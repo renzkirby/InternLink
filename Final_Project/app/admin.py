b07fda197bb3519f6ce1efd49979d70238e69d20
RenzWindows <117254869+renzkirby@users.noreply.github.com>
2025-12-13 11:59:48 +0800

    Migrate models and register models

--- Final_Project/app/admin.py
+++ Final_Project/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(StudentProfile)
+admin.site.register(CoordinatorProfile)
+admin.site.register(SupervisorProfile)
+admin.site.register(Company)
+admin.site.register(Internship)
+admin.site.register(StudentReport)
+admin.site.register(DailyLog)
+admin.site.register(Evaluation)
