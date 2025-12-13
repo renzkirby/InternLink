b6464d169c104a01b4b454b7e32693fe15e938e7
RenzWindows <117254869+renzkirby@users.noreply.github.com>
2025-12-13 15:14:36 +0800

    Update studentprofile model

--- Final_Project/Final_Project/settings.py
+++ Final_Project/Final_Project/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app.apps.AppConfig",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "app.User"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- Final_Project/app/models.py
+++ Final_Project/app/models.py
@@ -25,10 +25,10 @@ class StudentProfile(models.Model):
         on_delete=models.CASCADE,
         related_name="student_profile",
     )
-    student_number = models.CharField(max_length=50, unique=True)
-    course = models.CharField(max_length=100)
-    year_level = models.IntegerField()
-    contact_number = models.CharField(max_length=20)
+    student_number = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    course = models.CharField(max_length=100, null=True, blank=True)
+    year_level = models.IntegerField(null=True, blank=True)
+    contact_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.get_full_name()} - {self.student_number}"
