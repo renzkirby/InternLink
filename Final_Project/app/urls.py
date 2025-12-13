59c4c9e3bf9af81143614c5cdf3c0c50c74b9cee
RenzWindows <117254869+renzkirby@users.noreply.github.com>
2025-12-13 13:27:34 +0800

    Add register form view

--- Final_Project/app/urls.py
+++ Final_Project/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView, register_view
 
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
+    path("register/", register_view, name="register"),
 ]
--- Final_Project/app/views.py
+++ Final_Project/app/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import login
+from .forms import StudentRegistrationForm
 
 
 # Create your views here.
 class DashboardView(TemplateView):
     template_name = "app/dashboard.html"
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = StudentRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = StudentRegistrationForm()
+
+    return render(request, "app/register.html", {"form": form})
