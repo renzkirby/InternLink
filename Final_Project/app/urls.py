e7276562a6c254b8af687ffd6d4fe9aed08a57e2
RenzWindows <117254869+renzkirby@users.noreply.github.com>
2025-12-26 12:50:05 +0800

    Add student profile update

--- Final_Project/app/urls.py
+++ Final_Project/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DashboardView, register_view
+from .views import register_view
 from app import views
 from django.contrib.auth import views as auth_views
 
@@ -13,4 +13,7 @@ urlpatterns = [
     ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", auth_views.LoginView.as_view(template_name="app/login.html"), name="home"),
+    path(
+        "profile/update/", views.student_profile_update, name="student_profile_update"
+    ),
 ]
