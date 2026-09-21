e5ae38b01db8e1253c84d49193e7d699d717c6ec
RenzKirby <117254869+renzkirby@users.noreply.github.com>
2026-09-21 20:34:48 +0800

    Fix supervisor list JSON response

--- Final_Project/app/views.py
+++ Final_Project/app/views.py
@@ -766,5 +766,6 @@ def get_supervisors_for_company(request):
                 "name": f"{supervisor.user.get_full_name()} ({supervisor.company.name})",
             }
             for supervisor in supervisors
-        ]
+        ],
+        safe=False,
     )
